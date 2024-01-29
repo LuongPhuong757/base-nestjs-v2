48b9c106afaa24ac6be9ffc563cd36414cb3180f
Phuong.Luong <phuong.luong@sotatek.com>
2024-01-29 16:51:33 +0700

    final set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,25 +6,25 @@ import { database_config } from './configs/configuration.config';
 import * as Joi from 'joi';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test', 'provision', 'staging')
-          .default('development'),
-        PORT: Joi.number().default(3000),
-      }),
-      validationOptions: {
-        abortEarly: false, // abortEarly: false để hiển thị toàn bộ các biến môi trường có lỗi thay vì mặc định chỉ hiển thị biến đầu tiên
-      },
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-      load: [database_config],
-      cache: true, // <== Ở đây
-      expandVariables: true, // Option expandVariables giúp chúng ta truy cập vào một biến môi trường khác trong file env.
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			validationSchema: Joi.object({
+				NODE_ENV: Joi.string()
+					.valid('development', 'production', 'test', 'provision', 'staging')
+					.default('development'),
+				PORT: Joi.number().default(3000),
+			}),
+			validationOptions: {
+				abortEarly: false, // abortEarly: false để hiển thị toàn bộ các biến môi trường có lỗi thay vì mặc định chỉ hiển thị biến đầu tiên
+			},
+			isGlobal: true,
+			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+			load: [database_config],
+			cache: true, // <== Ở đây
+			expandVariables: true, // Option expandVariables giúp chúng ta truy cập vào một biến môi trường khác trong file env.
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
